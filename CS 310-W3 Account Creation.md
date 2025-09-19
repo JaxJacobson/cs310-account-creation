9964a682f417ae3e1e1dd4880a323b37dafd115c
JaxJacobson <jaxjacobson@gmail.com>
2025-09-19 16:29:49 +0000

    Create accounts for users using their inputted information

---
+++ CS 310-W3 Account Creation.md
@@ -0,0 +1,12 @@
+# Jax Jacobson
+# 9/19/25
+# CS 310-W3 Account Creation
+# Create accounts for users based on their inputs.
+#
+#
+## How did you invoke the useradd command line tool?
+# I had to import the subprocess library in order to use the useradd command line tool.
+#
+## How did you pass flags to the useradd command?
+# I had to use the subprocess.run() function which wrote directly to the terminal and turned out to be pretty
+# simple.
--- account_creation.py
+++ account_creation.py
@@ -1,29 +1,67 @@
-def main():
+# Jax Jacobson
+# 9/19/25
+# CS 310-W3 Account Creation
+# Create accounts for users based on their inputs.
 
-    def creation():
-        
-        name = input("Enter your First and Last Name: ")
-        username = name.replace(" ", "-")
-        role = input("Enter your role (Admin[A], User[B], AV Tech[C]): ")
+import subprocess
 
-        confirmation = input(f"You are {name}, your username is {username}, and your role is {role}. Is this correct? (Y/N): ")
+def create():
+    """Function used to create a user account and assign permissions based on user inputs."""
+
+    print()
+    name = input("Enter your First and Last Name: ")
+    username = name.replace(" ", "-")                                   # Replace spaces with hyphens in names
+    print()
+    role = input("Enter your role (Admin[A], User[B], AV Tech[C]): ")
+
+    if role == 'A' or role == 'a':
+        role = "Admin"
+        group = "sudo"
+    elif role == 'B' or role == 'b':
+        role = "User"
+        group = "None"
+    elif role == 'C' or role == 'c':
+        role = "AV Tech"
+        group = "video, audio"
+
+    print()
+    password = input("Enter your desired password: ")
+
+    print()
+    confirmation = input(f"You are {name}, your username is {username}, and your role is {role}. Is this correct? (Y/N): ")
+
+
+    if confirmation == 'N' or confirmation == 'n':                                      # Make sure the user is sure
+        create()
+
+    else:                                                                    
+        print()
+        print("Creating account...")
+        print()
+
+    subprocess.run(["sudo", "useradd", "-m", "-c", name, "-s", "/bin/bash", username])  # Types directly into the terminal to create user.
+    subprocess.run(["sudo", "chpasswd"], input = f"{username}:{password}".encode())     # Creates password and hashes it.
+                                                                                        # chpasswd takes stdin for passwd
     
+    if role == 'A' or role == 'a':
+        perm = subprocess.run(["sudo", "usermod", "-aG", group, username])              # Gives the user root permisions
+    elif role == 'B' or role == 'b':
+        perm = "None"                                                                   # Does not give the user anything
+    elif role == 'C' or role == 'c':
+        perm = subprocess.run(["sudo", "usermod", "-aG", group, username])              # Gives user video and audio group permissions
 
-        if confirmation == 'N' or confirmation == 'n':
-            creation()
+    print()
+    print(f"Account for {name} created successfully!")
 
-        else:
-            accounts = {
-                'username': username,
-                'name': name,
-                'role': role,
-                'Admin': ["Root"],
-                'User': ["No Additional Groups"],
-                'AV Tech': ["Audio", "Video"]
-        }
+    print()
+    print()
+    print(f"Name: {name}")
+    print(f"Username: {username}")
+    print(f"Permissions: {group}")
+    print()
 
-            print("Account successfully created!")
-            print(accounts)
+def main():
+    create()
 
 
 if __name__ == "__main__":
